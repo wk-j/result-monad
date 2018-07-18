c1a70c489622b0b9e2e77a3933be67cb1a3696db
wk-j <somnuk.wk@gmail.com>
2018-07-18 13:54:10 +0700

    Initial commit

---
+++ src/ResultMonad/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ResultMonad {
+    public struct Result<TData> {
+
+        public Result(TData data) {
+            Data = data;
+            Success = true;
+            Error = "";
+        }
+
+        public bool Success { private set; get; }
+        public TData Data { private set; get; }
+        public string Error { private set; get; }
+
+        public static Result<TData> FromError(string err) {
+            return new Result<TData> {
+                Success = false,
+                Error = err
+            };
+        }
+        public static Result<TData> FromData(TData data) {
+            return new Result<TData> {
+                Success = true,
+                Data = data
+            };
+        }
+    }
+
+    public static class Extensions {
+        public static Result<TResult> SelectMany<TSource, TCollection, TResult>(
+                this Result<TSource> source,
+                Func<TSource, Result<TCollection>> collectionSelector,
+                Func<TSource, TCollection, TResult> resultSelector) {
+
+            if (source.Success) {
+                var collection = collectionSelector(source.Data);
+                if (collection.Success) {
+                    var result = resultSelector(source.Data, collection.Data);
+                    return Result<TResult>.FromData(result);
+                } else {
+                    return Result<TResult>.FromError(collection.Error);
+                }
+            } else {
+                return Result<TResult>.FromError(source.Error);
+            }
+        }
+    }
+}
